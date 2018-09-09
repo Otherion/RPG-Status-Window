d934c29bd0a1d79a52c205ceedf45c98bc9c8b7c
Otherion <40878732+Otherion@users.noreply.github.com>
2018-09-09 12:51:10 -0400

    Moved int conversion and changed to form.Read()
    Changed from form.ReadNonBlocking() to form.Read() - more efficient and responsive when using PySimpleGui

--- StatusWindow.py
+++ StatusWindow.py
@@ -4,7 +4,7 @@ import PySimpleGUI as sg
 def status_window():
 
     sg.ChangeLookAndFeel('TealMono')  #Changes color scheme of window created
-    form = sg.FlexForm('Status Window', auto_size_text=True, auto_size_buttons=False, grab_anywhere=False)
+    form = sg.FlexForm('Status Window', auto_size_text=True, auto_size_buttons=False, grab_anywhere=False, return_keyboard_events=True)
     
     cname = sg.Text('', size=(20, 1))   # The name of your character
     start_class = sg.Text('', size=(12, 1), font=('Helvetica', 20), background_color='black', text_color='white', justification='center')
@@ -12,42 +12,58 @@ def status_window():
     #Classes are placeholder names and will change
     class_list = ['Warrior', 'Wizard', 'Rogue', 'Sword Mage', 'Mercenary', 'Sorcerer', 'All-Rounder']
 
-    layout = [[sg.Text('Name:'), cname, sg.RealtimeButton('Choose Name')],    #Layout for the status window
+    layout = [[sg.Text('Name:'), cname, sg.ReadFormButton('Choose Name')],  # Layout for the status window
               [sg.Text('_' * 55)],
               [sg.Text('Stats:')],
-              [sg.Text('STR:', size=(5, 1)), sg.Spin([i for i in range(0, 101)], initial_value=0, key='STR', size=(5, 1))],
-              [sg.Text('INT:', size=(5, 1)), sg.Spin([i for i in range(0, 101)], initial_value=0, key='INT', size=(5, 1))],
-              [sg.Text('DEX:', size=(5, 1)), sg.Spin([i for i in range(0, 101)], initial_value=0, key='DEX', size=(5, 1))],
-              [sg.Text('Class:', size=(5, 1), font=('Helvetica', 20)), start_class, sg.RealtimeButton('Class Info')],
+              [sg.Text('STR:', size=(5, 1)),
+               sg.Spin([i for i in range(0, 101)], initial_value=0, key='STR', size=(5, 1), change_submits=True)],
+              [sg.Text('INT:', size=(5, 1)),
+               sg.Spin([i for i in range(0, 101)], initial_value=0, key='INT', size=(5, 1), change_submits=True)],
+              [sg.Text('DEX:', size=(5, 1)),
+               sg.Spin([i for i in range(0, 101)], initial_value=0, key='DEX', size=(5, 1), change_submits=True)],
+              [sg.Text('Class:', size=(5, 1), font=('Helvetica', 20)), start_class, sg.ReadFormButton('Class Info')],
               [sg.Text(' ' * 34), sg.Exit()]]
 
-    form.LayoutAndRead(layout, non_blocking=True)
+    form.Layout(layout)
 
     while True:
-        button, values = form.ReadNonBlocking()
+        button, values = form.Read()
         
-        if button == 'Choose Name':
+        if button == 'Choose Name':                      # Button to type in your name for your character
             name = sg.PopupGetText('What is your name?')
             cname.Update(name)
         
-        if values is None or button == 'Exit': break                        #Program ends successfully if 'Quit' is clicked or window is closed
+        if button is None or button == 'Exit': break     # Program ends successfully if 'Quit' is clicked or window is closed
 
         #When no stat requirements are met:
-        if all([int(values['STR']), int(values['INT']), int(values['DEX'])]) < 10: start_class.Update('')
+        try:
+            strength = int(values['STR'])
+            intel = int(values['INT'])
+            dex = int(values['DEX'])
+        except:
+            continue
 
-        #Classes based on one stat:
-        if int(values['STR']) == 10: start_class.Update(class_list[0])
-        elif int(values['INT']) == 10: start_class.Update(class_list[1])      #Class is displayed in window when the stat requirements are met  (stat requirements are placeholder)
-        elif int(values['DEX']) == 10: start_class.Update(class_list[2])
+        if all((strength, intel, dex)) < 10: start_class.Update('')
 
-        #Classes based on two stats:
-        if int(values['STR']) >= 10 and int(values['INT']) >= 10: start_class.Update(class_list[3])
-        elif int(values['STR']) >= 10 and int(values['DEX']) >= 10: start_class.Update(class_list[4])
-        elif int(values['INT']) >= 10 and int(values['DEX']) >= 10: start_class.Update(class_list[5])
+        # Classes based on one stat:
+        if strength == 10:
+            start_class.Update(class_list[0])
+        elif intel == 10:
+            start_class.Update(class_list[1])  
+        elif dex == 10:
+            start_class.Update(class_list[2])
 
-        #Classes based on three stats:
-        if int(values['STR']) >= 10 and int(values['INT']) >= 10 and int(values['DEX']) >= 10: start_class.Update(class_list[6])
+        # Classes based on two stats:
+        if strength >= 10 and intel >= 10:
+            start_class.Update(class_list[3])
+        elif strength >= 10 and dex >= 10:
+            start_class.Update(class_list[4])
+        elif intel >= 10 and dex >= 10:
+            start_class.Update(class_list[5])
 
+        # Classes based on three stats:
+        if strength >= 10 and intel >= 10 and dex >= 10: start_class.Update(class_list[6])
+        
         #Class info button
         if button == 'Class Info' and start_class.DisplayText == '':
             sg.Popup('Not a Class:', 'If you see this, go back and obtain a class!')

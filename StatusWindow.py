b5f6f47d3dc935e138628b2be3eab1373c99cb63
Otherion <40878732+Otherion@users.noreply.github.com>
2018-09-09 14:23:33 -0400

    Added "Reset Stats" button to GUI
    The "Reset Stats" button resets the stats back the initial value (now 5 instead of 0) and resets the class name.

--- StatusWindow.py
+++ StatusWindow.py
@@ -7,22 +7,24 @@ def status_window():
     form = sg.FlexForm('Status Window', auto_size_text=True, auto_size_buttons=False, grab_anywhere=False, return_keyboard_events=True)
     
     cname = sg.Text('', size=(20, 1))   # The name of your character
-    start_class = sg.Text('', size=(12, 1), font=('Helvetica', 20), background_color='black', text_color='white', justification='center')
+    start_class = sg.Text('', size=(12, 1), font=('Helvetica', 20), background_color='black', text_color='white', 
+                          justification='center', key='class')
     
     #Classes are placeholder names and will change
     class_list = ['Warrior', 'Wizard', 'Rogue', 'Sword Mage', 'Mercenary', 'Sorcerer', 'All-Rounder']
 
+    istat = 5   # Initial stat value
     layout = [[sg.Text('Name:'), cname, sg.ReadFormButton('Choose Name')],  # Layout for the status window
               [sg.Text('_' * 55)],
               [sg.Text('Stats:')],
               [sg.Text('STR:', size=(5, 1)),
-               sg.Spin([i for i in range(0, 101)], initial_value=0, key='STR', size=(5, 1), change_submits=True)],
+               sg.Spin([i for i in range(0, 101)], initial_value=istat, key='STR', size=(5, 1), change_submits=True)],
               [sg.Text('INT:', size=(5, 1)),
-               sg.Spin([i for i in range(0, 101)], initial_value=0, key='INT', size=(5, 1), change_submits=True)],
+               sg.Spin([i for i in range(0, 101)], initial_value=istat, key='INT', size=(5, 1), change_submits=True)],
               [sg.Text('DEX:', size=(5, 1)),
-               sg.Spin([i for i in range(0, 101)], initial_value=0, key='DEX', size=(5, 1), change_submits=True)],
+               sg.Spin([i for i in range(0, 101)], initial_value=istat, key='DEX', size=(5, 1), change_submits=True)],
               [sg.Text('Class:', size=(5, 1), font=('Helvetica', 20)), start_class, sg.ReadFormButton('Class Info')],
-              [sg.Text(' ' * 34), sg.Exit()]]
+              [sg.ReadFormButton('Reset Stats'), sg.Text(' ' * 51), sg.Exit()]]
 
     form.Layout(layout)
 
@@ -63,6 +65,13 @@ def status_window():
 
         # Classes based on three stats:
         if strength >= 10 and intel >= 10 and dex >= 10: start_class.Update(class_list[6])
+        
+        # Button that resets stats back initial values as well as class name
+        if button == 'Reset Stats':
+            form.FindElement('STR').Update(istat)
+            form.FindElement('INT').Update(istat)
+            form.FindElement('DEX').Update(istat)
+            form.FindElement('class').Update('')
         
         #Class info button
         if button == 'Class Info' and start_class.DisplayText == '':

a71e7a6514aefae161fcc9a116c878f7ac26db23
Otherion <40878732+Otherion@users.noreply.github.com>
2018-09-12 20:31:55 -0400

    Updated "Magic Types" button and Wizard classes
    When you select a magic from the "Magic Types" button, a new wizard class will appear next to "Class:" in the window. Only applies to when stat requirements for the "Wizard" class are met so classes that require INT and STR or INT and DEX or all 3 do not have separate classes for each element at the moment.

--- StatusWindow.py
+++ StatusWindow.py
@@ -43,6 +43,7 @@ def status_window():
     form.FindElement('magic').Update(disabled=True)
    
     class_list = []
+    element = []
 
     while True:
         button, values = form.Read()
@@ -62,13 +63,42 @@ def status_window():
             continue
 
         if all((strength, intel, dex)) < 10: class_list.append('')
+        
+        if intel < 10 and len(element) > 0: 
+            del element[-1]
+            form.FindElement('magic').Update(disabled=True)
 
+        # What the 'Magic Type' button does 
+        if button == 'Magic Type':
+            emagic = magic_types()
+            if emagic[0] is True: element.append('Fire')
+            if emagic[1] is True: element.append('Ice')
+            if emagic[2] is True: element.append('Water')
+            if emagic[3] is True: element.append('Space')
+            if emagic[4] is True: element.append('Earth')
+            if emagic[5] is True: element.append('Time')
+            if emagic[6] is True: element.append('Wind')
+            if emagic[7] is True: element.append('Holy')
+            if emagic[8] is True: element.append('Electricity')
+            if emagic[9] is True: element.append('Shadow')
+        
         # Classes based on one stat:
         if strength >= 10:
             class_list.append('Warrior')
-        elif intel >= 10:
+        elif intel >= 10 and len(element) == 0:
             class_list.append('Wizard')
             form.FindElement('magic').Update(disabled=False)
+        elif intel >= 10 and len(element) != 0:
+            if element[-1] == 'Fire': class_list.append('Pyromancer')
+            elif element[-1] == 'Ice': class_list.append('Cryomancer')
+            elif element[-1] == 'Water': class_list.append('Hydromancer')
+            elif element[-1] == 'Space': class_list.append('Spacial Wizard')
+            elif element[-1] == 'Earth': class_list.append('Terra Mage')
+            elif element[-1] == 'Time': class_list.append('Chronomancer')
+            elif element[-1] == 'Wind': class_list.append('Sky Mage')
+            elif element[-1] == 'Holy': class_list.append('Divine Mage')
+            elif element[-1] == 'Electricity': class_list.append('Lightning Mage')
+            elif element[-1] == 'Shadow': class_list.append('Umbral Mage')
         elif dex >= 10:
             class_list.append('Rogue')
 
@@ -86,25 +116,22 @@ def status_window():
         # Classes based on three stats:
         if strength >= 10 and intel >= 10 and dex >= 10:
             class_list.append('All-Rounder')
-            
-        # What the 'Magic Type' button does and an example of it changing the class
-        if button == 'Magic Type':
-            values = magic_types()
-            if values[0] is True and class_list[-1] == 'Wizard':
-                class_list.append('Fire Wizard')
 
         # Button that resets stats back initial values as well as class name
         if button == 'Reset Stats':
             form.Fill({'STR': 5, 'INT': 5, 'DEX': 5})
             form.FindElement('magic').Update(disabled=True)
             class_list.clear(); class_list.append('')  # Clears the list then appends an empty string to it as the first item
-
+            element.clear()  # Clears the element list to an empty list
+            
         # The class displayed in the window will be the last item that was appended to the class_list
         form.FindElement('class').Update(class_list[-1])
         
-        # Makes the class list only one item (the last class selected)
+        # Makes the class list and element list only one item long
         if len(class_list) >= 2:
             del class_list[:-1]
+        if len(element) >= 2:
+            del element[:-1]
 
         # Class info button
         if button == 'Class Info' and class_list[-1] == '':
